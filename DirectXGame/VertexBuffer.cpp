306cb448f6cde145192424c978a03a5dc311f054
Kirvvs <jb.ursua13@gmail.com>
2024-10-17 17:24:19 +0800

    minor fix

--- DirectXGame/VertexBuffer.cpp
+++ DirectXGame/VertexBuffer.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "GraphicsEngine.h"
+#include "Vector3D.h"
 
 VertexBuffer::VertexBuffer():m_layout(0),m_buffer(0)
 {
@@ -34,7 +35,7 @@ bool VertexBuffer::load(void* list_vertices, UINT size_vertex, UINT size_list, v
 	D3D11_INPUT_ELEMENT_DESC layout[] =
 	{
 		{"POSITION", 0,  DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,D3D11_INPUT_PER_VERTEX_DATA ,0},
-		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(Vector3D), D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
 
 	UINT size_layout = ARRAYSIZE(layout);
